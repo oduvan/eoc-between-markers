243bbab0dd39b96ec88a396ae3e007db9c723cd5
Alexander Lyabah <a.lyabah@checkio.org>
2019-06-14 14:41:31 +0300

    cover

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -14,3 +14,18 @@ class Referee(RefereeRank):
         "python_3": "between_markers",
         "js_node": "betweenMarkers"
     }
+    ENV_COVERCODE = {
+        ENV_NAME.PYTHON: '''
+
+def cover(func, in_data):
+    return func(*in_data)
+
+    ''',
+            ENV_NAME.JS_NODE: '''
+
+function cover(func, in_data) {
+    return func.apply(this, in_data)
+}
+
+    '''
+    }
